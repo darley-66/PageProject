0039faae69aaa7e1a4821201ac00ab55b0e631e1
alejowot-blip <alejowot@gmail.com>
2026-09-04 12:55:43 -0500

    Add Machine Learning concepts and Use Case 1

--- app.py
+++ app.py
@@ -6,15 +6,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return  "Hello World"
+    return "Hello World"
 
 @app.route("/pagina/")
 def pagina():
     return render_template("index.html")
 
-@app.route("/LinearRegression/", methods =["GET","POST"])
+@app.route("/concepts/")
+def concepts():
+    return render_template("concepts.html")
+
+@app.route("/LinearRegression/", methods=["GET", "POST"])
 def calculate():
     calculateResult = None
     hours = requests
     calculateResult = LinearRegression.calculateGrade(5)
-    return render_template("temLinearRegression.html", result = calculateResult)
+    return render_template("temLinearRegression.html", result=calculateResult)
+
+if __name__ == "__main__":
+    app.run(debug=True)
